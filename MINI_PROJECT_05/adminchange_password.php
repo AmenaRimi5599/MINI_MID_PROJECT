0ae64470494808b7dcc994b37d6463110b60e1f6
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-08-11 21:44:18 +0600

    admin password

---
+++ MINI_PROJECT_05/adminchange_password.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+  
+</head>
+<body >
+	<form  >
+  <fieldset >
+    <legend>CHANGE PASSWORD</legend>
+    <table width="40%" cellspacing="5">
+      <tr>
+        <td>Current Password</td>
+        <td>: <input type="text" name="currpass"></td>
+      </tr>
+      <tr>
+        <td>New Password</td>
+        <td>: <input type="Password" name="newpass"></td>
+      </tr>
+      <tr>
+        <td>Retype New Password</td>
+        <td>: <input type="Password" name="rnewpass"></td>
+      </tr>
+    </table>
+    <hr><br>
+    <input type="submit" name="submit" value="change">
+    <a href="admin.php">Home</a>
+  </fieldset>
+</form>
+</body>
+</html>
