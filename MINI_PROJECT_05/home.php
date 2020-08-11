29477feb506969d814d9032aa9423644448d975f
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-08-11 21:44:45 +0600

    home

---
+++ MINI_PROJECT_05/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+</head>
+<body>
+	<form>
+		
+		
+			<table border="1" cellspacing="0" cellpadding="5" align="center" width="40%">
+				<tr>
+					<td  align="left" colspan="2">
+						<h3>Xcompany</h3>
+						<ul align="right">
+							<a href="login.php">Login</a>
+						<a href="registration.php">Registration</a>
+						</ul>
+					</td>
+				</tr>
+				<tr>
+					<td align="center" colspan="2">
+						<h1>Welcome</h1>
+					</td>
+				</tr>
+			</table>
+		
+	</form>
+</body>
+</html>
