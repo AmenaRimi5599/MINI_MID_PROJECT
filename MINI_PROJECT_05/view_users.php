e8042cc76dbcfcbb8492bfe3923922e0445f59d4
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-08-11 21:49:00 +0600

    view users

---
+++ MINI_PROJECT_05/view_users.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Users Page</title>
+</head>
+<body>
+	<form>
+		
+			<table border="1" cellspacing="0" cellpadding="5" align="center">
+				<tr>
+					<td align="center" colspan="4"><h3>Users</h3></td>
+				</tr>
+				<tr>
+					<td width="120px">ID</td>
+					<td width="80px">NAME</td>
+					<td width="180px">EMAIL</td>
+					<td>USER TYPE</td>
+				</tr>
+				<tr>
+					<td height="20px"></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr >
+					<td height="20px"></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr >
+					<td height="20px"></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr >
+					<td height="20px"></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td colspan="4" align="right"><a href="admin.php">Go Home</a></td>
+				</tr>
+			</table>
+		
+	</form>
+</body>
+</html>
