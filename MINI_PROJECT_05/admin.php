f2554bfa4d4c9815cce2427da7ccc7213eadc64f
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-08-11 21:42:44 +0600

    admin page

--- MINI_PROJECT_05/admin.php
+++ MINI_PROJECT_05/admin.php
@@ -1,10 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>ADMIN page</title>
+	<title>ADMIN Page</title>
 </head>
 <body>
-	<h1>Welcome Admin</h1>
-	<table 
+	<form>
+		
+			<table border="1" cellspacing="0" cellpadding="5" align="center" width="40%">
+			<tr>
+				<td align="center"><h1>Welcome Admin!</h1>
+				</td>
+			</tr>
+			<tr>
+				<td align="center" height="30%">
+					<p><a href="admin_profile.php"> Profile</a></p>
+					<p><a href="adminchange_password.php">Change Password</a></p>
+					<p><a href="view_users.php">View Users</a></p>
+					<p><a href="login.php">logout</a></p>
+				</td>
+			</tr>
+		
+		</table>
+	</form>
 </body>
 </html>
