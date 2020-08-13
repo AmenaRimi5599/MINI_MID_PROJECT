7c25dc051ef175f5c4a898d6e2eeb155335b3c86
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-08-13 11:02:13 +0600

    validate

--- MINI_PROJECT_05/regcheck.php
+++ MINI_PROJECT_05/regcheck.php
@@ -1,18 +1,30 @@
 
 <?php
-    if(isset($_POST['submit'])){
-    	$conn = mysqli_connect('localhost', 'root', '', 'mini_project');
+    
 
         $name     = $_POST['name'];
 	    $userid   =  $_POST['userId'];
 	    $email    =$_POST['email'];
 	    $password =  $_POST['pass'];
         $usertype = $_POST['userType'];
+
+        if(isset($_POST['submit'])){
+        $conn = mysqli_connect('localhost', 'root', '', 'mini_project');
     
         $query = "INSERT INTO userinfo (Name, UserId, Email, Password, UserType) VALUES ('$name', '$userid', '$email', '$password','$usertype')";
-        $result = mysqli_query($conn, $query);
+
+        if(empty($name) || empty( $userid ) || empty($email ) || empty( $password) || empty($usertype))
+        {
+
+            echo "Invalid";
+        }
+        else
+        {
+             $result = mysqli_query($conn, $query);
 
         header('location: login.php');
+        }
+       
     }
     else
     {
